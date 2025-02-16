08d24443004cc028814f9f428722b5a32a60a8ea
johncarlo-dizon <johncarlovdizon@gmail.com>
2025-02-17 00:30:00 +0800

    commit by jc

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Sample profile data
+  final List<Map<String, String>> profiles = [
+    {
+      "name": "John Carlo V. Dizon",
+      "address": "Dolores Piring Mexico",
+      "phone": "+63 912 345 6789",
+      "email": "john.dizon@email.com",
+      "image": "assets/jc.png"
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: SafeArea(
+        child: ListView.separated(
+          itemCount: profiles.length,
+          separatorBuilder: (context, index) => Container(
+            margin: EdgeInsets.symmetric(horizontal: 15),
+            child: Divider(
+              color: CupertinoColors.systemGrey,
+              thickness: 1,
+            ),
+          ),
+          itemBuilder: (context, index) {
+            var profile = profiles[index];
+
+            return Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              decoration: BoxDecoration(
+                color: CupertinoColors.systemGrey6,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Profile Image
+                  ClipOval(
+                    child: Image.asset(
+                      profile["image"]!,
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(width: 15),
+
+                  // Profile Information
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Name: ${profile['name']}",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Text("Address: ${profile['address']}"),
+                        Text("Phone: ${profile['phone']}"),
+                        Text("Email: ${profile['email']}"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
